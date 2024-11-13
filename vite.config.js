22c1aef5ba09c1cae9b392730e0a12ee994ea3f4
myogaads <m.yogaadi1234@gmail.com>
2024-11-13 07:24:47 +0700

    Update : History Call Back API

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
@@ -13,9 +13,6 @@ export default defineConfig({
         },
       },
     },
-    chunkSizeWarningLimit: 3000, // Adjust the limit as needed
-  },
-  server: {
-    historyApiFallback: true,
+    chunkSizeWarningLimit: 3000,
   },
 });
